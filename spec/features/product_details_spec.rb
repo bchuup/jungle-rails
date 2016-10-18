5627d8552449add0404af60c4e35be2f9a2ced77
bchuup <bchuup@gmail.com>
2016-10-18 18:10:51 +0000

    testing clicking to details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details" do
+    # ACT
+    visit root_path
+    click_on 'Details'
+
+    # DEBUG / VERIFY
+    sleep 1
+    save_screenshot
+
+    expect(page).to have_content 'Quantity'
+  end
+end
